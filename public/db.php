0a884ec16c43e468163ad0b561b4bf89ba95e332
SAMURAI JACK <mr.amir1385mm@gmail.com>
2025-04-05 22:40:12 +0330

    modified subscription buyuing
    modified buying subscription options.

--- public/db.php
+++ public/db.php
@@ -61,6 +61,92 @@ class DB
         return $stmt->fetch(PDO::FETCH_OBJ);
     }
 
+    public function updateWallet($user_id, $balance, $updated_at, $last)
+    {
+        $stmt = $this->db->prepare("UPDATE wallet SET updated_at = :updated_at , balance=:balance , last_depo = :last_depo WHERE user_id = :user_id");
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->bindParam(':balance', $balance);
+        $stmt->bindParam(':updated_at', $updated_at);
+        $stmt->bindParam(':last_depo', $last);
+        $stmt->execute();
+    }
+
+    public function createSub($user_id, $username,$g,$e)
+    {
+
+        $curl = curl_init();
+
+        $time = time();
+        // Dynamic data
+        $data = [
+            "data_limit" => $g,
+            "data_limit_reset_strategy" => "no_reset",
+            "expire" => $e,
+            "inbounds" => [
+                "vless" => [
+                    "VLESS TCP REALITY",
+                    "VLESS GRPC REALITY"
+                ]
+            ],
+            "next_plan" => [
+                "add_remaining_traffic" => false,
+                "data_limit" => 0,
+                "expire" => 0,
+                "fire_on_either" => true
+            ],
+            "note" => "",
+            "on_hold_expire_duration" => 0,
+            "on_hold_timeout" => "2023-11-03T20:30:00",
+            "proxies" => [
+                "vless" => new stdClass() // Empty object
+            ],
+            "status" => "active",
+            "username" => "$username-$time"
+        ];
+
+        // Convert the PHP array to a JSON string
+        $postFields = json_encode($data);
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://nn.nayathemitherkabnafhnkahf.ir/api/user',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $postFields, // Use the JSON-encoded data
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJOYXlhVlBOIiwiYWNjZXNzIjoic3VkbyIsImlhdCI6MTc0Mzg1MDI0MywiZXhwIjoxNzQzOTM2NjQzfQ.EHN_HHqUOr2o11JAVc4FLL5_C8M5UGGlUCmxPih5I4k'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $response = json_decode($response);
+
+
+
+
+        $stmt = $this->db->prepare("INSERT INTO subs (user_id,sub_url,exp_date,data_limit) VALUES (:user_id,:sub_url,:exp_date,:data_limit)");
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->bindParam(':sub_url', $response->subscription_url);
+        $stmt->bindParam(':exp_date', $response->expire);
+        $stmt->bindParam(':data_limit', $response->data_limit);
+        $stmt->execute();
+    }
+
+    public function getSub($user_id)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM subs WHERE user_id = :user_id");
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
     public function deleteUser($chat_id)
     {
         $stmt = $this->db->prepare("DELETE FROM users WHERE chat_id = :chat_id");
--- public/index.php
+++ public/index.php
@@ -75,10 +75,12 @@ if ($text == "/user_ac") {
 
     $option = array(
         //First row
-        array($telegram->buildInlineKeyBoardButton("💵 کیف پول", '', '/user_wallet')),
+        array($telegram->buildInlineKeyBoardButton('🚀 کانفیگ های من', '', '/user_subs')),
         //Second row
-        array($telegram->buildInlineKeyBoardButton("💰 کسب درآمد", '', '/refferal')),
+        array($telegram->buildInlineKeyBoardButton("💵 کیف پول", '', '/user_wallet')),
         //Third row
+        array($telegram->buildInlineKeyBoardButton("💰 کسب درآمد", '', '/refferal')),
+        //Fourth row
         array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/home'))
     );
     $keyb = $telegram->buildInlineKeyBoard($option);
@@ -109,6 +111,95 @@ if ($text == "/user_wallet") {
     $telegram->editMessageText($content);
 }
 
+if ($text == "/deposit") {
+    $validCommand = true;
+
+    $option = array(
+        //First row
+        array($telegram->buildInlineKeyBoardButton('💵 20000 تومان', '', '/deposited')),
+        //Second row
+        array($telegram->buildInlineKeyBoardButton('💵 50000 تومان', '', '/deposited')),
+        //Third row
+        array($telegram->buildInlineKeyBoardButton('💵 100000 تومان', '', '/deposited')),
+        //Fourth row
+        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/user_wallet'))
+    );
+    $keyb = $telegram->buildInlineKeyBoard($option);
+
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => 'افزایش موجودی کیف پول 💰' . "\n\n" . 'لطفا مبلغ مورد نظر را انتخاب کنید.' . "\n\n" . 'گزینه های بیشتر 👇', 'message_id' => $result['callback_query']['message']['message_id']);
+    $telegram->editMessageText($content);
+}
+
+if ($text == "/deposited") {
+    $validCommand = true;
+
+    $option = array(
+        //First row
+        array($telegram->buildInlineKeyBoardButton('💠 پشتیبانی', 'https://t.me//NayaVPN_Support', '')),
+        //Second row
+        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/user_wallet'))
+    );
+    $keyb = $telegram->buildInlineKeyBoard($option);
+
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => 'افزایش موجودی کیف پول 💰' . "\n\n" . 'برای افزایش موجودی , مبلغ را به شماره کارت زیر واریز کرده و رسید پرداختی را برای پشتیبانی ارسال کنید.' . "\n\n" . '0000 0000 0000 0000', 'message_id' => $result['callback_query']['message']['message_id']);
+    $telegram->editMessageText($content);
+}
+// if ($text == "/deposit_20000") {
+//     $validCommand = true;
+
+//     $new = new DB($db);
+//     $user = $new->getUser($chat_id);
+//     $wallet = $new->getWallet($user->id);
+
+//     $new->updateWallet($user->id, $wallet->balance + 20000, time());
+
+//     $option = array(
+//         //First row
+//         array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/user_wallet'))
+//     );
+//     $keyb = $telegram->buildInlineKeyBoard($option);
+
+//     $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => 'افزایش موجودی کیف پول 💰' . "\n\n" . 'موجودی شما با موفقیت افزایش یافت.' . "\n\n" . '💳 موجودی فعلی : ' . ($wallet->balance + 10000) . "\n" . '📥 اخرین واریز شما : ' . date('Y-m-d H:i:s', time()), 'message_id' => $result['callback_query']['message']['message_id']);
+//     $telegram->editMessageText($content);
+// }
+// if ($text == "/deposit_50000") {
+//     $validCommand = true;
+
+//     $new = new DB($db);
+//     $user = $new->getUser($chat_id);
+//     $wallet = $new->getWallet($user->id);
+
+//     $new->updateWallet($user->id, $wallet->balance + 50000, time());
+
+//     $option = array(
+//         //First row
+//         array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/user_wallet'))
+//     );
+//     $keyb = $telegram->buildInlineKeyBoard($option);
+
+//     $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => 'افزایش موجودی کیف پول 💰' . "\n\n" . 'موجودی شما با موفقیت افزایش یافت.' . "\n\n" . '💳 موجودی فعلی : ' . ($wallet->balance + 20000) . "\n" . '📥 اخرین واریز شما : ' . date('Y-m-d H:i:s', time()), 'message_id' => $result['callback_query']['message']['message_id']);
+//     $telegram->editMessageText($content);
+// }
+// if ($text == "/deposit_100000") {
+//     $validCommand = true;
+
+//     $new = new DB($db);
+//     $user = $new->getUser($chat_id);
+//     $wallet = $new->getWallet($user->id);
+
+//     $new->updateWallet($user->id, $wallet->balance + 100000, time());
+
+//     $option = array(
+//         //First row
+//         array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/user_wallet'))
+//     );
+//     $keyb = $telegram->buildInlineKeyBoard($option);
+
+//     $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => 'افزایش موجودی کیف پول 💰' . "\n\n" . 'موجودی شما با موفقیت افزایش یافت.' . "\n\n" . '💳 موجودی فعلی : ' . ($wallet->balance + 50000) . "\n" . '📥 اخرین واریز شما : ' . date('Y-m-d H:i:s', time()), 'message_id' => $result['callback_query']['message']['message_id']);
+//     $telegram->editMessageText($content);
+// }
+
+
 if ($text == "/refferal") {
     $validCommand = true;
 
@@ -117,12 +208,38 @@ if ($text == "/refferal") {
 
     $option = array(
         //First row
-        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/home'))
+        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/home')),
     );
     $keyb = $telegram->buildInlineKeyBoard($option);
 
     $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => 'طرح کسب درآمد نایا وی پی ان 🤑' . "\n\n" . 'شما در نایا وی پی ان میتوانید با دعوت از دوستان خود , از خرید آنها به اعتبار حساب خود بیافزایید. لینک رفرال شما و تعداد رفرال های شما به صورت زیر است:'
-        . "\n\n" . '👥 رفرال های شما :'. $user->refs . "\n" . '📡 لینک رفرال شما : ' . "https://t.me/nayavpn_shopbot?start=$user->ref_id" . "\n\n", 'message_id' => $result['callback_query']['message']['message_id']);
+        . "\n\n" . '👥 رفرال های شما :' . $user->refs . "\n" . '📡 لینک رفرال شما : ' . "https://t.me/nayavpn_shopbot?start=$user->ref_id" . "\n\n", 'message_id' => $result['callback_query']['message']['message_id']);
+    $telegram->editMessageText($content);
+}
+
+if ($text == "/user_subs") {
+    $validCommand = true;
+
+    $new = new DB($db);
+    $user = $new->getUser($chat_id);
+
+    $subs = $new->getSub($user->id);
+
+    if (isset($subs)) {
+        foreach ($subs as $sub) {
+            $content .= '💳 پلن شما : ' . $sub->sub_url . "\n" . '📅 تاریخ انقضا : ' . date('Y-m-d H:i:s', $sub->exp_date) . "\n\n".var_dump($sub);
+        }
+    } else {
+        $content = 'شما هیچ کانفیگی ندارید.';
+    }
+
+    $option = array(
+        //First row
+        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/home'))
+    );
+    $keyb = $telegram->buildInlineKeyBoard($option);
+
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => 'کانفیگ های من 📡' . "\n\n" . "$content", 'message_id' => $result['callback_query']['message']['message_id']);
     $telegram->editMessageText($content);
 }
 
@@ -147,6 +264,208 @@ if ($text == "/buy_sub") {
 
     $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => 'خرید کانفیگ جدید 💵' . "\n\n" . 'پلن های زیر , پلن های پیشنهادی نایا وی پی ان هستن. شما همچنین میتونید پلن دلخواه خودتون رو از طریق دکمه پلن سفارشی بسازید!'
         . "\n\n" . 'انتخاب پلن 👇', 'message_id' => $result['callback_query']['message']['message_id']);
+    $telegram->editMessageText($content);
+}
+
+
+if ($text == "/5G1M") {
+    $validCommand = true;
+
+    $option = array(
+        //First row
+        array($telegram->buildInlineKeyBoardButton('💵 خرید', '', '/buy_confirm_5g1m')),
+        //Second row
+        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/buy_sub'))
+    );
+    $keyb = $telegram->buildInlineKeyBoard($option);
+    $content = array(
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyb,
+        'text' => 'خرید پلن 5 گیگابایت 1 ماهه' . "\n\n" . 'قیمت : 10000 تومان' . "\n\n" . 'هزینه خرید از اعتبار شما کسر خواهد شد. در صورت کمبود اعتبار , از منوی کیف پول اقدام به افزایش اعتبار کنید.',
+        'message_id' => $result['callback_query']['message']['message_id']
+    );
+    $telegram->editMessageText($content);
+}
+if ($text == "/10G1M") {
+    $validCommand = true;
+
+    $option = array(
+        //First row
+        array($telegram->buildInlineKeyBoardButton('💵 خرید', '', '/buy_10G1M')),
+        //Second row
+        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/buy_sub'))
+    );
+    $keyb = $telegram->buildInlineKeyBoard($option);
+    $content = array(
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyb,
+        'text' => 'خرید پلن 10 گیگابایت 1 ماهه' . "\n\n" . 'قیمت : 20000 تومان' . "\n\n" . 'هزینه خرید از اعتبار شما کسر خواهد شد. در صورت کمبود اعتبار , از منوی کیف پول اقدام به افزایش اعتبار کنید.',
+        'message_id' => $result['callback_query']['message']['message_id']
+    );
+    $telegram->editMessageText($content);
+}
+if ($text == "/25G1M") {
+    $validCommand = true;
+
+    $option = array(
+        //First row
+        array($telegram->buildInlineKeyBoardButton('💵 خرید', '', '/buy_25G1M')),
+        //Second row
+        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/buy_sub'))
+    );
+    $keyb = $telegram->buildInlineKeyBoard($option);
+    $content = array(
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyb,
+        'text' => 'خرید پلن 25 گیگابایت 1 ماهه' . "\n\n" . 'قیمت : 50000 تومان' . "\n\n" . 'هزینه خرید از اعتبار شما کسر خواهد شد. در صورت کمبود اعتبار , از منوی کیف پول اقدام به افزایش اعتبار کنید.',
+        'message_id' => $result['callback_query']['message']['message_id']
+    );
+    $telegram->editMessageText($content);
+}
+if ($text == "/50G1M") {
+    $validCommand = true;
+
+    $option = array(
+        //First row
+        array($telegram->buildInlineKeyBoardButton('💵 خرید', '', '/buy_50G1M')),
+        //Second row
+        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/buy_sub'))
+    );
+    $keyb = $telegram->buildInlineKeyBoard($option);
+    $content = array(
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyb,
+        'text' => 'خرید پلن 50 گیگابایت 1 ماهه' . "\n\n" . 'قیمت : 80000 تومان' . "\n\n" . 'هزینه خرید از اعتبار شما کسر خواهد شد. در صورت کمبود اعتبار , از منوی کیف پول اقدام به افزایش اعتبار کنید.',
+        'message_id' => $result['callback_query']['message']['message_id']
+    );
+    $telegram->editMessageText($content);
+}
+if ($text == "/60G2M") {
+    $validCommand = true;
+
+    $option = array(
+        //First row
+        array($telegram->buildInlineKeyBoardButton('💵 خرید', '', '/buy_60G2M')),
+        //Second row
+        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/buy_sub'))
+    );
+    $keyb = $telegram->buildInlineKeyBoard($option);
+    $content = array(
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyb,
+        'text' => 'خرید پلن 60 گیگابایت 2 ماهه' . "\n\n" . 'قیمت : 110000 تومان' . "\n\n" . 'هزینه خرید از اعتبار شما کسر خواهد شد. در صورت کمبود اعتبار , از منوی کیف پول اقدام به افزایش اعتبار کنید.',
+        'message_id' => $result['callback_query']['message']['message_id']
+    );
+    $telegram->editMessageText($content);
+}
+if ($text == "/100G2M") {
+    $validCommand = true;
+
+    $option = array(
+        //First row
+        array($telegram->buildInlineKeyBoardButton('💵 خرید', '', '/buy_100G2M')),
+        //Second row
+        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/buy_sub'))
+    );
+    $keyb = $telegram->buildInlineKeyBoard($option);
+    $content = array(
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyb,
+        'text' => 'خرید پلن 100 گیگابایت 2 ماهه' . "\n\n" . 'قیمت : 150000 تومان' . "\n\n" . 'هزینه خرید از اعتبار شما کسر خواهد شد. در صورت کمبود اعتبار , از منوی کیف پول اقدام به افزایش اعتبار کنید.',
+        'message_id' => $result['callback_query']['message']['message_id']
+    );
+    $telegram->editMessageText($content);
+}
+if ($text == "/120G3M") {
+    $validCommand = true;
+
+    $option = array(
+        //First row
+        array($telegram->buildInlineKeyBoardButton('💵 خرید', '', '/buy_120G3M')),
+        //Second row
+        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/buy_sub'))
+    );
+    $keyb = $telegram->buildInlineKeyBoard($option);
+    $content = array(
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyb,
+        'text' => 'خرید پلن 120 گیگابایت 3 ماهه' . "\n\n" . 'قیمت : 180000 تومان' . "\n\n" . 'هزینه خرید از اعتبار شما کسر خواهد شد. در صورت کمبود اعتبار , از منوی کیف پول اقدام به افزایش اعتبار کنید.',
+        'message_id' => $result['callback_query']['message']['message_id']
+    );
+    $telegram->editMessageText($content);
+}
+if ($text == "/200G3M") {
+    $validCommand = true;
+
+    $option = array(
+        //First row
+        array($telegram->buildInlineKeyBoardButton('💵 خرید', '', '/buy_200G3M')),
+        //Second row
+        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/buy_sub'))
+    );
+    $keyb = $telegram->buildInlineKeyBoard($option);
+    $content = array(
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyb,
+        'text' => 'خرید پلن 200 گیگابایت 3 ماهه' . "\n\n" . 'قیمت : 220000 تومان' . "\n\n" . 'هزینه خرید از اعتبار شما کسر خواهد شد. در صورت کمبود اعتبار , از منوی کیف پول اقدام به افزایش اعتبار کنید.',
+        'message_id' => $result['callback_query']['message']['message_id']
+    );
+    $telegram->editMessageText($content);
+}
+
+if ($text == "/buy_confirm_5g1m") {
+    $validCommand = true;
+    $g = 5;
+    $m = 1;
+    $e = $m * time() + 60 * 60 * 24 * 30;
+    $new = new DB($db);
+    $user = $new->getUser($chat_id);
+    $wallet = $new->getWallet($user->id);
+    $price = 0 * $g; // Assuming 10000 Toman for 5GB
+    if ($wallet->balance >= $price) {
+
+        $new->updateWallet($user->id, $wallet->balance - $price, time(), $price);
+
+        $new->createSub($user->id, $user->username, $g, $e);
+
+
+        $option = array(
+            //First row
+            array($telegram->buildInlineKeyBoardButton('💵 خرید', '', '/buy_confirm' . "?g=5&m=1")),
+            //Second row
+            array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/buy_sub'))
+        );
+        $keyb = $telegram->buildInlineKeyBoard($option);
+        $content = array(
+            'chat_id' => $chat_id,
+            'reply_markup' => $keyb,
+            'text' => 'خرید شما با موفقیت انجام شد.' . "\n" . 'مبلغ : ' . $price . ' تومان' . "\n" . 'پلن : ' . $g . ' گیگابایت , ' . $m . ' ماهه' . "\n" . 'تاریخ انقضا : ' . date('Y-m-d H:i:s', time() + 60 * 60 * 24 * 30 * $m),
+            'message_id' => $result['callback_query']['message']['message_id']
+        );
+        $telegram->editMessageText($content);
+    } else {
+        $content = array('chat_id' => $chat_id, 'text' => 'موجودی کیف پول شما کافی نیست.' . "\n" . 'لطفا از منوی کیف پول اقدام به افزایش اعتبار کنید.', 'message_id' => $result['callback_query']['message']['message_id']);
+        $telegram->editMessageText($content);
+    }
+}
+
+
+if ($text == "/custom_sub") {
+    $validCommand = true;
+
+    $option = array(
+        //First row
+        array($telegram->buildInlineKeyBoardButton('💵 خرید', '', '/buy_custom')),
+        //Second row
+        array($telegram->buildInlineKeyBoardButton('🔙 بازگشت', '', '/buy_sub'))
+    );
+    $keyb = $telegram->buildInlineKeyBoard($option);
+    $content = array(
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyb,
+        'text' => 'خرید پلن سفارشی' . "\n\n" . 'شما میتوانید پلن دلخواه خود را با توجه به نیاز خود بسازید. برای این کار , از دکمه خرید استفاده کنید.' . "\n\n" . 'انتخاب پلن 👇',
+        'message_id' => $result['callback_query']['message']['message_id']
+    );
     $telegram->editMessageText($content);
 }
 
